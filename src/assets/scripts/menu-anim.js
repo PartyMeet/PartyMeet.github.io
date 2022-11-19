ab1791ef9636ba84830c2932b8e0ccb13b5b41fa
Maksymilian Walicki <maxwalicki@gmail.com>
2022-11-19 20:20:30 +0100

    Add - section design, parallax effects, fade animations

--- src/assets/scripts/menu-anim.js
+++ src/assets/scripts/menu-anim.js
@@ -1,20 +1,37 @@
 $(function(){
-
-    let path = document.querySelector('path');
-    let pathLength = path.getTotalLength();
-    path.style.strokeDasharray = pathLength + ' ' + pathLength;
-    path.style.strokeDashoffset = pathLength;
-
-    // draw path on scroll
-    window.addEventListener('scroll', ()=>{
-        var scrollPercentage = (-190 + document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-        var drawLength = pathLength * scrollPercentage;
-        path.style.strokeDashoffset = pathLength - drawLength;
-        
-    })
-
+    // FADE ON SCROLL
+    $(window).on("load",function() {
+        $(window).scroll(function() {
+          var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+          $(".fade").each(function() {
+            /* Check the location of each desired element */
+            var objectBottom = $(this).offset().top + $(this).outerHeight();
+            var direction = $(this).attr('direction') == 'left' ? 'translateX(-50px)' : 'translateX(50px)';
+            
+            /* If the element is completely within bounds of the window, fade it in */
+            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+              if ($(this).css("opacity")==0) {$(this).css('opacity', '1').css('transform', 'translateX(0)');}
+            } else { //object goes out of view (scrolling up)
+              if ($(this).css("opacity")==1) {$(this).css('opacity', '0').css('transform', direction);}
+            }
+          });
+        }).scroll(); //invoke scroll-handler on page-load
+    });
     
+    // PARALLAX ON SCROLL
+    $(document).scroll(function(){
+        var scrollPos = $(this).scrollTop();
+        this.querySelectorAll('.parallax-item').forEach(layer => {
+            
 
+            var speed = $(layer).attr('data-speed');
+            $(layer).css('margin-top', scrollPos/speed);
+        });
+        
+    });
+    
+    
+    // PARALLAX ON MOUSE MOVE
     document.addEventListener("mousemove", parallax)
     function parallax(e) {
         this.querySelectorAll('.parallax').forEach(layer => { // img parallax move
@@ -26,4 +43,17 @@ $(function(){
             layer.style.transform = `translateX(${x}px) translateY(${y}px) translate(-50%, -50%)`
         })
     }
+    
+    // let path = document.querySelector('path');
+    // let pathLength = path.getTotalLength();
+    // path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    // path.style.strokeDashoffset = pathLength;
+
+    // // draw path on scroll
+    // window.addEventListener('scroll', ()=>{
+    //     var scrollPercentage = (-190 + document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    //     var drawLength = pathLength * scrollPercentage;
+    //     path.style.strokeDashoffset = pathLength - drawLength;
+        
+    // })
 })
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,25 @@
 import React from "react"
+import PhoneImg from '../assets/img/logo_phone.png'
+
 
 export function About(){
     return(
-        <div class="container">
-            <section class="z-20 h-screen w-screen flex-auto bg-white p-5">
-                
+        <div className="container about">
+            {/* Innowacyjność */}
+            <section className="bg-white w-screen h-screen">
+                <h1 className="parallax-item font-bold text-9xl relative text-slate-100" data-speed='3'>INNOWACJE</h1>
+                <h2 className="font-thin relative text-6xl text-[#161616]">Innowacje</h2>
+                <div className="flex md:flex-row flex-col relative section-box">
+                    <div className="m-10">
+                        <img className="fade parallax-item" data-speed='8' src={PhoneImg} />
+                    </div>
+                    <div className="p-6 md:p-0 md:ml-10 w-screen text-center md:w-72 md:text-left text-[#161616] text-2xl font-light">
+                        <p className="fade" direction="left">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.    
+                        </p> 
+                    </div>
+                </div>
             </section>
-            <section class="h-screen w-screen bg-white p-5 text-[#161616]">hello2</section>
         </div>
 
     )
